6b37e148f293377562fa15ba9a4614d482861eb6
moinulwebdev <moinulwebdeveloper@gmail.com>
2026-09-10 05:13:58 +0600

    added hook pre-commit

--- hook.py
+++ hook.py
@@ -1,20 +1,4 @@
-def calculate_total():
-    price = 100
-    quantity = 5
-    total = price * quantity
-    print(total
+cars = ["Ford", "Volvo", "BMW"]
 
-
-def greet_user(name)
-    message = "Hello " + name
-    print(message)
-
-
-def unused_function():
-    x = 10
-    y = 20
-    return
-
-
-calculate_total()
-greet_user("Moinul")
+for x in cars:
+    print(x)
