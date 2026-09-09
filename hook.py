483dca5ca9092f960d48cd6bbfd5e13beb3da589
moinulwebdev <moinulwebdeveloper@gmail.com>
2026-09-10 04:13:03 +0600

    test hook

---
+++ hook.py
@@ -0,0 +1,20 @@
+def calculate_total():
+    price = 100
+    quantity = 5
+    total = price * quantity
+    print(total
+
+
+def greet_user(name)
+    message = "Hello " + name
+    print(message)
+
+
+def unused_function():
+    x = 10
+    y = 20
+    return
+
+
+calculate_total()
+greet_user("Moinul")
